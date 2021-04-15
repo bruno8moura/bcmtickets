83a9bd328ec1180abb38bec56adc89e3380687cf
Bruno Moura <bruno8moura@gmail.com>
2021-04-15 05:40:40 -0300

    Updates

--- src/util/rightousTimezone/index.ts
+++ src/util/rightousTimezone/index.ts
@@ -1,7 +1,8 @@
 export const getTime = (expirationWindowSeconds: number) => {
     const now = new Date();
-    const rightousTime = new Date(now.getTimezoneOffset() > 0 ? now.setHours(now.getHours() - (now.getTimezoneOffset() / 60)) : now.setHours(now.getHours() + (now.getTimezoneOffset() / 60)));
-    const result = new Date(rightousTime.setSeconds(rightousTime.getSeconds() + expirationWindowSeconds));
-    
-    return result;
+    const timezoneLocalTime = now.getTimezoneOffset() > 0 ? now.setHours(now.getHours() - (now.getTimezoneOffset() / 60)) : now.setHours(now.getHours() + (now.getTimezoneOffset() / 60));
+    now.setTime(timezoneLocalTime);
+    now.setSeconds(now.getSeconds() + expirationWindowSeconds);
+        
+    return now;
 };
